74634bf37e597844cbd18e1f3df8f9d1bb466bfa
Vishal <vishal.cloudus@gmail.com>
2022-10-19 15:22:07 +0530

    Update AppManage.java

--- AdsSdk/src/main/java/com/pesonal/adsdk/AppManage.java
+++ AdsSdk/src/main/java/com/pesonal/adsdk/AppManage.java
@@ -1252,7 +1252,7 @@ public class AppManage {
             }
         }
         editor_count.commit();
-
+//comemmenc
         ArrayList<String> placemnt_Ids = new ArrayList<String>();
         for (int i = 0; i < platform_Format_Ids.length; i++) {
             if (!platform_Format_Ids[i].isEmpty()) {
